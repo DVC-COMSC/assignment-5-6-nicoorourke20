c9913667f9a244a6ccb3aafd347f49c16c9e262f
Kyuwoong Lee <kwooler@gmail.com>
2022-10-11 08:18:33 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+## Assignment 5-5:Lab 5-12
+
+
+![Question 3](https://nimbus-screenshots.s3.amazonaws.com/s/177175c899d91b7ef34b88732d223be4.png)
+
+
+
---
+++ main.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N; // the number of students;
+	int M; // the number of scores;
+
+	cout << "Enter the number of students: " ;
+	cin >> N;
+	cout << "Enter the number of scores for each student: " ;
+	cin >> M;
+	
+	for(int i=0; i<N; i++)
+	{
+		for (int j=0; j<M; j++)
+		{
+			
+		}
+	}
+
+	
+	// Complete this code.
+	
+}
