c7b4d370cb7a552a8c88a9476c162f5beab89b5c
Kyu Lee <kwooler@gmail.com>
2023-09-17 17:18:06 -0700

    CS120A56

--- main.cpp
+++ main.cpp
@@ -4,15 +4,10 @@ using namespace std;
 
 int main()
 {
-	int N=3;
-	int M=3;
-	
-	for(int i=0; i<N; i++)
-	{
-		for (int j=0; j<M; j++)
-		{
-			// Complete this code.
-			
-		}
-	}
+	int N;
+	cin >> N;
+
+	/***************************************************
+	 * Code Your program here
+	 ***************************************************/
 }
