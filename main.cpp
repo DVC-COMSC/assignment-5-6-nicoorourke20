4e0f22db853367d85182c56782b2efe60b24c5d9
nicoorourke20 <157660744+nicoorourke20@users.noreply.github.com>
2024-03-08 22:46:54 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-using namespace std;
 
-int main()
-{
-    int N;
-    cin >> N;
+int main() {
+    int n;
+    std::cout << "Enter the size of the rectangle: ";
+    std::cin >> n;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = i; j < N; j++) {
-            cout << i << "," << j << endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = i; j < n; ++j) {
+            std::cout << i << "," << j << std::endl;
         }
     }
-
     return 0;
 }
