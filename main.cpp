ce5054c14f5eb5d2cf50241cb640c1437b13728c
nicoorourke20 <157660744+nicoorourke20@users.noreply.github.com>
2024-03-08 22:43:32 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	int N;
-	cin >> N;
+    int N;
+    cin >> N;
 
-	/***************************************************
-	 * Code Your program here
-	 ***************************************************/
+    for (int i = 0; i < N; i++) {
+        for (int j = i; j < N; j++) {
+            cout << i << "," << j << endl;
+        }
+    }
+
+    return 0;
 }
