8014ebe40ed79a74bc0601103577c1502eac6e6f
nicoorourke20 <157660744+nicoorourke20@users.noreply.github.com>
2024-03-08 22:56:03 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            if (j >= i && j < n - 1) {
+            if (j >= i) {
                 std::cout << i << "," << j << std::endl;
             }
         }
